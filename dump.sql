3997164e314cc141bc88054ee8269de7f50629ec
antonhusky <114214937+antonhusky@users.noreply.github.com>
2023-02-05 18:10:56 +0300

    added db transaction

---
+++ dump.sql
@@ -0,0 +1,43 @@
+
+CREATE TABLE genre_table (
+    `id` INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(250) NOT NULL
+);
+CREATE TABLE artist_table (
+    `id` INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(250) NOT NULL
+);
+CREATE TABLE album_table (
+    `id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(250) NOT NULL
+);
+CREATE TABLE tracks_table (
+    `id` INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(250) NOT NULL,
+    `album_id` INT(11) NOT NULL,
+    `duration` INT(11) NOT NULL,
+    FOREIGN KEY (`album_id`) REFERENCES album_table(`id`)
+);
+CREATE TABLE collection_table (
+    `id` INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(250) NOT NULL,
+    `year` INT(11) NOT NULL
+);
+CREATE TABLE collection_track_binding_table (
+    `collection_id` INT(11) NOT NULL,
+    `track_id` INT(11) NOT NULL,
+    FOREIGN KEY (`collection_id`) REFERENCES collection_table(`id`),
+    FOREIGN KEY (`track_id`) REFERENCES tracks_table(`id`)
+);
+CREATE TABLE artist_genre_binding_table (
+    `artist_id` INT(11) NOT NULL,
+    `genre_id` INT(11) NOT NULL,
+    FOREIGN KEY (`artist_id`) REFERENCES artist_table(`id`),
+    FOREIGN KEY (`genre_id`) REFERENCES genre_table(`id`)
+);
+CREATE TABLE album_artist_binding_table (
+    `album_id` INT(11) NOT NULL,
+    `artist_id` INT(11) NOT NULL,
+    FOREIGN KEY (`album_id`) REFERENCES album_table(`id`),
+    FOREIGN KEY (`artist_id`) REFERENCES artist_table(`id`)
+);
